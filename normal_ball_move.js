adc63b1ff405b70a215970e427245be88cc0cc79
yoursaint <yoursaint@cnu.ac.kr>
2019-01-07 16:09:16 +0900

    A ball can be bounce

--- normal_ball_move.js
+++ normal_ball_move.js
@@ -5,21 +5,40 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10 ,0, Math.PI * 2); // x, y is center of circle
+    ctx.arc(x, y, ballRadius ,0, Math.PI * 2); // x, y is center of circle
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath(); // ctx closed
 }
 
+function moveCirclesX() {
+    x += dx;
+
+    if (x > (canvas.width - ballRadius) || x< ballRadius) {
+        dx = -dx;
+    } 
+}
+
+function moveCirclesY() {
+    y += dy;
+
+    if (y > (canvas.height - ballRadius) || y < ballRadius) {
+        dy = - dy;
+    }
+}
+
 function moveBall() {
     ctx.clearRect(0,0, canvas.width, canvas.height); // clear every component in this rectangle
     drawBall();
-    x += dx;
-    y += dy;
+    moveCirclesX();
+    moveCirclesY();
 }// when this function call, circle will be moved
+
+
 
 setInterval(moveBall, 10); // when every 10 millsec, this function going to be called
 
