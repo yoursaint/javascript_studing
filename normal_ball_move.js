1608d7629a33baa68144ca1f7c2896c58a756465
yoursaint <yoursaint@cnu.ac.kr>
2019-01-07 15:48:31 +0900

    new js, for ball movings

---
+++ normal_ball_move.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d"); // canvas 2d rendering context saved in ctx
+
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10 ,0, Math.PI * 2); // x, y is center of circle
+    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle();
+    ctx.closePath(); // ctx closed
+    x += dx;
+    y += dy;
+}// when this function call, circle will be moved
+
+setInterval(draw, 10); // when every 10 millsec, this function going to be called
+
