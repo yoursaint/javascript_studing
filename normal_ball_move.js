f310d65b6b598a4d4f674217dce59904d4f8ba84
yoursaint <yoursaint@cnu.ac.kr>
2019-01-07 15:58:13 +0900

    modification functions clearly

--- normal_ball_move.js
+++ normal_ball_move.js
@@ -6,16 +6,20 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-function draw() {
-    ctx.clearRect(0,0, canvas.width, canvas.height); // clear every component in this rectangle
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 10 ,0, Math.PI * 2); // x, y is center of circle
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath(); // ctx closed
+}
+
+function moveBall() {
+    ctx.clearRect(0,0, canvas.width, canvas.height); // clear every component in this rectangle
+    drawBall();
     x += dx;
     y += dy;
 }// when this function call, circle will be moved
 
-setInterval(draw, 10); // when every 10 millsec, this function going to be called
+setInterval(moveBall, 10); // when every 10 millsec, this function going to be called
 
