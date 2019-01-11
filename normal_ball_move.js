90e115ed9bcdab41176000a869424b8af9d5b377
yoursaint <yoursaint@cnu.ac.kr>
2019-01-11 17:14:12 +0900

    Tracking and updating the status in the collision detction function

--- normal_ball_move.js
+++ normal_ball_move.js
@@ -31,7 +31,7 @@ var bricks = [];
 for(var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for(var r = 0; r < brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0};
+        bricks[c][r] = { x: 0, y: 0, status: 1};
     }
 }
 
@@ -39,15 +39,17 @@ for(var c = 0; c < brickColumnCount; c++) {
 function drawBricks() {
     for(var c = 0; c < brickColumnCount; c++) {
         for(var r = 0; r < brickRowCount; r++) {
-            var brickX = (c*(brickWidth + brickPadding)) + brickoffsetLeft;
-            var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+            if (bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth + brickPadding)) + brickoffsetLeft;
+                var brickY = (r*(brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
         }
     }
 }
@@ -58,9 +60,11 @@ function collisionDetection() {
     for(var c= 0; c < brickColumnCount; c++) {
         for(var r = 0; r < brickRowCount; r++) {
             var b = bricks[c][r];
-
-            if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-                dy = -dy;
+            if(b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                }
             }
         }
     }
